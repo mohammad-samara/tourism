1c2a15e15adc6ef2183d94115259363511375020
mohammad samara <mohammad.ab.samara@gmail.com>
2020-04-30 15:49:34 +0300

    add java

--- js/budjet.js
+++ js/budjet.js
@@ -1,13 +1,26 @@
 'use strict'
-var budjet = prompt ('enter yor budjet in numbers');
-if (budjet>=150) {
+var budjet1 = prompt('enter yor budjet in numbers');
+
+if (budjet1 >= 150) {
     document.write('<h3>' + 'we advide you to assighn for Turkey trip' + '</h3>');
+}
+    if (budjet1 >= 50 && budjet1<150) {
+        document.write('<h3>' + 'we advide you to assighn for aqaba trip' + '</h3>');
+    }
+        if (budjet1 >= 0 && budjet1<50)
+            document.write('<h3>' + 'you are too poor so we will give you a free trip to wadi rum' + '</h3>');
+     else {
+         if (budjet1<0) {
+            document.write('<h3>' + 'invalid entery , you have lost your chance for greet offers' + '</h3>');  
+         }
+        
+    } ;
+
+
+var confirm1 = confirm("are you doing good today?");
+
+if (!confirm1) {
+    alert("i hope you get better");
 } else {
-    if (budjet>=50) {
-        document.write('<h3>' + 'we advide you to assighn for aqaba trip' + '</h3>');    
-} else {
-    if (budjet>=0)
-        document.write('<h3>' + 'you are too poor so we will give you a free trip to wadi rum' + '</h3>');    
-} else {
-    document.write('<h3>' + 'invalid entery , you have lost your chance for greet offers' + '</h3>'); 
-};
+    alert("thats great");
+}
